5b9bbf01d5976500c9a1a81868f6a0543903e6a0
Cee1414 <kai971493@gmail.com>
2024-02-23 16:40:55 -0600

    user attribute graph progress

--- screen1/screen1.js
+++ screen1/screen1.js
@@ -1,5 +1,2 @@
-var elements = document.getElementsByClassName("hello");
-for (var i = 0; i < elements.length; i++) {
-    elements[i].innerHTML = "Hello, world from JavaScript!";
-  }
-  
+
+  console.log("hello world")
