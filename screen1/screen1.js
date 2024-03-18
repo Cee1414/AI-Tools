358ddce7620df8129563fbe8493588dbac910aee
Cee1414 <kai971493@gmail.com>
2024-03-18 15:13:23 -0500

    updated text

--- screen1/screen1.js
+++ screen1/screen1.js
@@ -1,25 +1,23 @@
 
-  console.log("hello world")
+  // console.log("hello world")
 
   function clickHandler() {
+
     createOverlay();
 
     document.body.removeEventListener("click", clickHandler);
 
   }
 
+
   function createOverlay() {
 
   var newDiv = document.createElement("div");
     
-    // Add a class name to the div
     newDiv.className = "dark-overlay";
     
-    // Append the new div to the body of the HTML
     document.body.appendChild(newDiv);
 
-
-    
   }
 
-  document.body.addEventListener("click", clickHandler);
+  document.body.addEventListener("click", clickHandler); 
