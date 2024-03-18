52f50482dcda33bf94f5030ea3add609e704c4cb
Cee1414 <kai971493@gmail.com>
2024-03-18 15:08:54 -0500

    function to add overlay for game overscreen on click

--- screen1/screen1.js
+++ screen1/screen1.js
@@ -1,2 +1,25 @@
 
   console.log("hello world")
+
+  function clickHandler() {
+    createOverlay();
+
+    document.body.removeEventListener("click", clickHandler);
+
+  }
+
+  function createOverlay() {
+
+  var newDiv = document.createElement("div");
+    
+    // Add a class name to the div
+    newDiv.className = "dark-overlay";
+    
+    // Append the new div to the body of the HTML
+    document.body.appendChild(newDiv);
+
+
+    
+  }
+
+  document.body.addEventListener("click", clickHandler);
