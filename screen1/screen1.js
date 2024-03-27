cf3e343661c7a34b4d3d2e4d8ba973a69645bfa2
Cee1414 <kai971493@gmail.com>
2024-03-27 15:00:59 -0500

    temporarily commented out js

--- screen1/screen1.js
+++ screen1/screen1.js
@@ -1,132 +1,132 @@
 
-//dark overlay functions
-  function clickHandlerDarkOverlay() {
+// //dark overlay functions
+//   function clickHandlerDarkOverlay() {
 
-    createDarkOverlay();
+//     createDarkOverlay();
 
-    document.body.removeEventListener("click", clickHandlerDarkOverlay);
+//     document.body.removeEventListener("click", clickHandlerDarkOverlay);
 
-  }
+//   }
 
 
-  function createDarkOverlay() {
+//   function createDarkOverlay() {
 
-  var newDiv = document.createElement("div");
+//   var newDiv = document.createElement("div");
     
-    newDiv.className = "dark-overlay";
+//     newDiv.className = "dark-overlay";
     
-    document.body.appendChild(newDiv);
+//     document.body.appendChild(newDiv);
 
-  }
+//   }
 
-  document.body.addEventListener("click", clickHandlerDarkOverlay); 
+//   document.body.addEventListener("click", clickHandlerDarkOverlay); 
 
-  document.body.addEventListener("click", clickHandlerGameOverScreen); //click event listeners 
+//   document.body.addEventListener("click", clickHandlerGameOverScreen); //click event listeners 
 
 
-//GameOverScreen Functions
+// //GameOverScreen Functions
 
-  function clickHandlerGameOverScreen() {
+//   function clickHandlerGameOverScreen() {
 
-    createGameOverScreen();
+//     createGameOverScreen();
 
-    document.body.removeEventListener("click", clickHandlerGameOverScreen);
+//     document.body.removeEventListener("click", clickHandlerGameOverScreen);
 
-  }
+//   }
 
-  function createGameOverScreen() {
+//   function createGameOverScreen() {
 
-    var gameOverBoxDiv = document.createElement("div");
-  gameOverBoxDiv.className = "game-over-box";
+//     var gameOverBoxDiv = document.createElement("div");
+//   gameOverBoxDiv.className = "game-over-box";
 
-  var gameOverMainContentDiv = document.createElement("div");
-  gameOverMainContentDiv.className = "game-over-main-content";
+//   var gameOverMainContentDiv = document.createElement("div");
+//   gameOverMainContentDiv.className = "game-over-main-content";
 
-  var yourScoreDiv = document.createElement("div");
-  yourScoreDiv.className = "your-score";
-  var yourScoreText = document.createTextNode("your score: ");
-  var scoreValue = document.createElement("p");
-  scoreValue.textContent = "54";
-  yourScoreDiv.appendChild(yourScoreText);
-  yourScoreDiv.appendChild(scoreValue);
+//   var yourScoreDiv = document.createElement("div");
+//   yourScoreDiv.className = "your-score";
+//   var yourScoreText = document.createTextNode("your score: ");
+//   var scoreValue = document.createElement("p");
+//   scoreValue.textContent = "54";
+//   yourScoreDiv.appendChild(yourScoreText);
+//   yourScoreDiv.appendChild(scoreValue);
 
-  var watchtimeLineDiv = document.createElement("div");
-  watchtimeLineDiv.className = "watchtime-line";
-  var watchtimeText = document.createElement("p");
-  watchtimeText.textContent = "Watchtime";
-  var watchtimeBarDiv = document.createElement("div");
-  watchtimeBarDiv.className = "watchtime-bar";
-  var watchtimeBarText = document.createElement("p");
-  watchtimeBarText.textContent = "5:24 / 10:00";
-  watchtimeBarDiv.appendChild(watchtimeBarText);
-  watchtimeLineDiv.appendChild(watchtimeText);
-  watchtimeLineDiv.appendChild(watchtimeBarDiv);
+//   var watchtimeLineDiv = document.createElement("div");
+//   watchtimeLineDiv.className = "watchtime-line";
+//   var watchtimeText = document.createElement("p");
+//   watchtimeText.textContent = "Watchtime";
+//   var watchtimeBarDiv = document.createElement("div");
+//   watchtimeBarDiv.className = "watchtime-bar";
+//   var watchtimeBarText = document.createElement("p");
+//   watchtimeBarText.textContent = "5:24 / 10:00";
+//   watchtimeBarDiv.appendChild(watchtimeBarText);
+//   watchtimeLineDiv.appendChild(watchtimeText);
+//   watchtimeLineDiv.appendChild(watchtimeBarDiv);
 
-  var likeLineDiv = document.createElement("div");
-  likeLineDiv.className = "like-line";
-  var likeText = document.createElement("p");
-  likeText.textContent = "Like?";
-  var likeBarDiv = document.createElement("div");
-  likeBarDiv.className = "like-bar";
-  var likeBarText = document.createElement("p");
-  likeBarText.textContent = "YES";
-  likeBarDiv.appendChild(likeBarText);
-  likeLineDiv.appendChild(likeText);
-  likeLineDiv.appendChild(likeBarDiv);
+//   var likeLineDiv = document.createElement("div");
+//   likeLineDiv.className = "like-line";
+//   var likeText = document.createElement("p");
+//   likeText.textContent = "Like?";
+//   var likeBarDiv = document.createElement("div");
+//   likeBarDiv.className = "like-bar";
+//   var likeBarText = document.createElement("p");
+//   likeBarText.textContent = "YES";
+//   likeBarDiv.appendChild(likeBarText);
+//   likeLineDiv.appendChild(likeText);
+//   likeLineDiv.appendChild(likeBarDiv);
 
-  var commentLineDiv = document.createElement("div");
-  commentLineDiv.className = "comment-line";
-  var commentText = document.createElement("p");
-  commentText.textContent = "Comment?";
-  var commentBarDiv = document.createElement("div");
-  commentBarDiv.className = "comment-bar";
-  var commentBarText = document.createElement("p");
-  commentBarText.textContent = "NO";
-  commentBarDiv.appendChild(commentBarText);
-  commentLineDiv.appendChild(commentText);
-  commentLineDiv.appendChild(commentBarDiv);
+//   var commentLineDiv = document.createElement("div");
+//   commentLineDiv.className = "comment-line";
+//   var commentText = document.createElement("p");
+//   commentText.textContent = "Comment?";
+//   var commentBarDiv = document.createElement("div");
+//   commentBarDiv.className = "comment-bar";
+//   var commentBarText = document.createElement("p");
+//   commentBarText.textContent = "NO";
+//   commentBarDiv.appendChild(commentBarText);
+//   commentLineDiv.appendChild(commentText);
+//   commentLineDiv.appendChild(commentBarDiv);
 
-  gameOverMainContentDiv.appendChild(yourScoreDiv);
-  gameOverMainContentDiv.appendChild(watchtimeLineDiv);
-  gameOverMainContentDiv.appendChild(likeLineDiv);
-  gameOverMainContentDiv.appendChild(commentLineDiv);
+//   gameOverMainContentDiv.appendChild(yourScoreDiv);
+//   gameOverMainContentDiv.appendChild(watchtimeLineDiv);
+//   gameOverMainContentDiv.appendChild(likeLineDiv);
+//   gameOverMainContentDiv.appendChild(commentLineDiv);
 
-  var bottomDiv = document.createElement("div");
-  bottomDiv.className = "bottom";
-  var nextButtonDiv = document.createElement("div");
-  nextButtonDiv.className = "next-button";
-  nextButtonDiv.textContent = "NEXT";
-  bottomDiv.appendChild(nextButtonDiv);
+//   var bottomDiv = document.createElement("div");
+//   bottomDiv.className = "bottom";
+//   var nextButtonDiv = document.createElement("div");
+//   nextButtonDiv.className = "next-button";
+//   nextButtonDiv.textContent = "NEXT";
+//   bottomDiv.appendChild(nextButtonDiv);
 
-  gameOverBoxDiv.appendChild(gameOverMainContentDiv);
-  gameOverBoxDiv.appendChild(bottomDiv);
+//   gameOverBoxDiv.appendChild(gameOverMainContentDiv);
+//   gameOverBoxDiv.appendChild(bottomDiv);
 
-  document.body.appendChild(gameOverBoxDiv);
+//   document.body.appendChild(gameOverBoxDiv);
 
-  }
+//   }
 
 
   
-//user object declarations MAX:100
+// //user object declarations MAX:100
 
-let user = {
+// let user = {
 
-    funny: 100,
-    smart: 50,
-    happy: 34,
-    sad: 16,
-    angry: 90,
-}
+//     funny: 100,
+//     smart: 50,
+//     happy: 34,
+//     sad: 16,
+//     angry: 90,
+// }
 
-//video object declarations MAX: 100
+// //video object declarations MAX: 100
 
-  let video = {
+//   let video = {
 
-    videoName: "DogPark",
-    funny: 87,
-    smart: 64,
-    happy: 77,
-    sad: 91,
-    angry: 33,
+//     videoName: "DogPark",
+//     funny: 87,
+//     smart: 64,
+//     happy: 77,
+//     sad: 91,
+//     angry: 33,
 
-  };
+//   };
