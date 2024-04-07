5ec1e17a40ce38998603f1b936dac3ebcb588eee
Cee1414 <kai971493@gmail.com>
2024-04-07 02:05:42 -0500

    implementing form for collecting name

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms import ValidationError
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.config.from_pyfile('../instance/config.py')
+
+secret_key = app.config['SECRET_KEY']
 
 # Configure SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
@@ -12,10 +21,32 @@ class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
+##handle forms 
 
-@app.route('/')
+def validateName(form, field):
+    name = field.data.split()
+    if len(name) < 2:
+        raise ValidationError('Please enter your full name (first name and last initial).')
+
+class lowerCaseStringField(StringField):
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = valuelist[0].lower()
+
+class nameForm(FlaskForm):
+    name = lowerCaseStringField('Input First Name And Last Initial:', validators=[DataRequired(), validateName])
+    submit = SubmitField('Submit')
+
+##handle routes
+
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    #process form data
+    form = nameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        return f'Hello, {name}!'
+    return render_template('index.html', form=form)
 
 @app.route('/screen1')
 def screen1():
