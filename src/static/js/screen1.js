30411bb4d4ab4b5070f9d1ede57aeca937e73ae5
Cee1414 <kai971493@gmail.com>
2024-04-13 23:44:01 -0500

    ensure videos don't repeat on same screen

--- src/static/js/screen1.js
+++ src/static/js/screen1.js
@@ -1,9 +1,9 @@
 import allVideos from './videos.js'
 
-let video1 = allVideos[0];
-let video2 = allVideos[1];
-let video3 = allVideos[2];
-let video4 = allVideos[3];
+let video1 = allVideos[randomVideoNum()];
+let video2 = allVideos[randomVideoNum()];
+let video3 = allVideos[randomVideoNum()];
+let video4 = allVideos[randomVideoNum()];
 
 function updateVideoImage(videoID, videoUrl) {
         
@@ -61,14 +61,43 @@ function updateAllVideos() {
 function randomizeVideoObjects() {
     //todo check if attribute is already taken or if same as prev video
 
+    //todo refactor while loop
+    let prevUrl = video1.url
     video1 = allVideos[randomVideoNum()];
+    while (video1.url == prevUrl || (video1.url == video2.url) || (video1.url == video3.url) || (video1.url == video4.url)){
+        video1 = allVideos[randomVideoNum()];
+    }
+    prevUrl = video2.url
+    while (video2.url == prevUrl || (video2.url == video1.url) || (video2.url == video3.url) || (video2.url == video4.url)){
     video2 = allVideos[randomVideoNum()];
+    }
+    prevUrl = video3.url
+    while (video3.url == prevUrl || (video3.url == video1.url) || (video3.url == video2.url) || (video3.url == video4.url)){
     video3 = allVideos[randomVideoNum()];
+    }
+    prevUrl = video4.url
+    while (video4.url == prevUrl || (video4.url == video1.url) || (video4.url == video3.url) || (video4.url == video2.url)){
     video4 = allVideos[randomVideoNum()];
+    }
 }
 
 
 //initialize videos
+
+while ((video2.url == video1.url) || (video2.url == video3.url) || (video2.url == video4.url)){
+    video2 = allVideos[randomVideoNum()];
+    }
+
+while ((video3.url == video1.url) || (video3.url == video2.url) || (video3.url == video4.url)){
+    video3 = allVideos[randomVideoNum()];
+}
+
+while ((video3.url == video1.url) || (video3.url == video2.url) || (video3.url == video4.url)){
+    video3 = allVideos[randomVideoNum()];
+}    
+
+
+
 updateVideoImage('vid1', video1.url);
 updateVideoImage('vid2', video2.url);
 updateVideoImage('vid3', video3.url);
